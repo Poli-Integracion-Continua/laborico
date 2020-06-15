c92bb08c80270935f70e73f4d5609819fd93ea5d
Andres Verjan <desbog5@gmail.com>
2020-06-15 11:27:16 -0500

    Se agrega vista de registro para los usuarios

--- views/header.php
+++ views/header.php
@@ -24,7 +24,7 @@
                   <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Registro</a>
+                  <a class="nav-link" href="registro.php">Registro</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#">Categorias</a>

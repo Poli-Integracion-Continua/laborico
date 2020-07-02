df109820f862e743f5a93dbed51673a356106d40
naverjan <andresver2040@gmail.com>
2020-07-02 16:02:06 -0500

    Ajuste en footer - small

--- views/footer.php
+++ views/footer.php
@@ -3,7 +3,7 @@
 <div id="footer">
 <footer id="sticky-footer" class="py-4 bg footer-laborico text-white-50">
     <div class="container text-center text-bold">
-      <stronge>Copyright &copy; Laborico 2020</stronge>
+      <small>Copyright &copy; Laborico 2020</small>
     </div>
   </footer>
   </div>

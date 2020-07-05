080c0369fa1a400b5da696c9e7ad62dccad3b3ba
lerochar1 <lerochar1@poligran.edu.co>
2020-07-05 12:42:20 -0500

    Update profesionesAdmin.php
    agregar listado de profesiones

--- LaboricoTravis/model/profesiones/profesionesAdmin.php
+++ LaboricoTravis/model/profesiones/profesionesAdmin.php
@@ -27,6 +27,48 @@ function getProsiones(){
     );
     array_push($arrayProfesiones, $prof1);
 
+    $prof2 = array(
+        'id' => 2,
+        'profesion' => 'Contador Publico',
+        'imagen' => 'contador.jpg'
+    );
+    array_push($arrayProfesiones, $prof2);
+
+	$prof3 = array(
+        'id' => 3,
+        'profesion' => 'Enfermeras(os)',
+        'imagen' => 'Enfermeras.jpg'
+    );
+    array_push($arrayProfesiones, $prof3);
+	
+	$prof4 = array(
+        'id' => 4,
+        'profesion' => 'Construcción',
+        'imagen' => 'Construccion.jpg'
+    );
+    array_push($arrayProfesiones, $prof4);
+
+	$prof5 = array(
+        'id' => 5,
+        'profesion' => 'Diseñadores',
+        'imagen' => 'disenador.jpg'
+    );
+    array_push($arrayProfesiones, $prof5);
+
+	$prof6 = array(
+        'id' => 6,
+        'profesion' => 'Confección',
+        'imagen' => 'Confeccion.jpg'
+    );
+    array_push($arrayProfesiones, $prof6);	
+	
+	$prof7 = array(
+        'id' => 7,
+        'profesion' => 'Asesores Comerciales',
+        'imagen' => 'vendedor.jpg'
+    );
+    array_push($arrayProfesiones, $prof7);
+
     //Retornamos la lista
     return $arrayProfesiones;
 }

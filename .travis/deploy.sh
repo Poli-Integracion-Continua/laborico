26f557e7a9cd7888a911243cef58c477b3bd26c1
yasanchezco <yasanchezco@poligran.edu.co>
2020-07-06 14:12:53 -0500

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-set -e
 sshpass -p 'InteGr@ci0n' ssh root@198.199.65.103
 # pull most current version of example web site container image
 docker pull naverjan/laborico:travis
-
-# stop and remove current container instance
-# docker rm -f exsite 
-
 # run instance of newly-updated container image
 docker run -p 80:80 naverjan/laborico:travis

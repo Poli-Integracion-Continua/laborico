9e498a3b05ff75e4f4f093f898b6eafcebf01333
naverjan <andresver2040@gmail.com>
2020-07-05 20:28:15 -0500

    Ajuste en pagina de profesiones mostrar 2 profesione por columnna

--- LaboricoTravis/categorias.php
+++ LaboricoTravis/categorias.php
@@ -7,21 +7,25 @@
 ?>
 
 <div class="container">
+  <div class="row mt-4">
     <?php foreach($profesiones as $value){ ?>
-    <div class="card mb-3 mt-3 shadow-div" style="max-width: 540px;">
-    <div class="row no-gutters">
-        <div class="col-md-4">
-        <img src="resources/images/<?= $value['imagen'] ?>" class="card-img" alt="...">
+     <div class="col-6">
+      <div class="card mb-3 mt-3 shadow-div" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+            <img src="resources/images/<?= $value['imagen'] ?>" class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title"><?=$value['profesion']?></h5>            
+                <a href="#" class="btn btn-primary">Ver Talentos</a>
+            </div>
+            </div>
         </div>
-        <div class="col-md-8">
-        <div class="card-body">
-            <h5 class="card-title"><?=$value['profesion']?></h5>            
-            <a href="#" class="btn btn-primary">Ver Talentos</a>
-        </div>
-        </div>
-    </div>
-    </div>
+      </div>
+     </div>
     <?php } ?>
+  </div>
 </div>
 
 <?php

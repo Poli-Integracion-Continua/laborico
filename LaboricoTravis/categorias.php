0c0e1f8499d1d5309f1a281a7814659700875bb5
naverjan <andresver2040@gmail.com>
2020-07-06 22:02:47 -0500

    Ajuste en boton profesionales - (prueba travis)

--- LaboricoTravis/categorias.php
+++ LaboricoTravis/categorias.php
@@ -18,7 +18,7 @@
             <div class="col-md-8">
             <div class="card-body">
                 <h5 class="card-title"><?=$value['profesion']?></h5>            
-                <a href="#" class="btn btn-primary">Ver Talentos</a>
+                <a href="#" class="btn btn-primary">Ver Profesionales</a>
             </div>
             </div>
         </div>
